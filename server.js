ed09d6c2679d0bfd09677f0b3f4f50b7defca138
Don <donabrams@gmail.com>
2014-02-24 19:18:17 -0500

    add log formatter to figure out whats wrong with heroku today

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
+var logfmt = require("logfmt");
 var port = Number(process.env.PORT || 5000);
 var app = express();
+
+app.use(logfmt.requestLogger());
 
 app.get("/", function(req, res) {
 	res.send(200, "yay");
