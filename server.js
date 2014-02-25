e88ad83acbf1bdf189aabb3a03a627dd2b0f5f66
Don <donabrams@gmail.com>
2014-02-24 19:10:36 -0500

    basic server for heroku integration

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("YAY NODE");
+var express = require("express");
+var port = Number(process.env.PORT || 5000);
+var app = express();
+
+app.get("/", function(req, res) {
+	res.status(200, "yay");
+});
+
+app.listen(port);
